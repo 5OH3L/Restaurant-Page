d4e51047f4e56f84cbd808814db64dd246002b9c
Sohel <152502004+5OH3L@users.noreply.github.com>
2025-04-05 23:06:37 +0530

    Load home page when nav home button is clicked

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,16 @@
-import "../styles/styles.css"
+import "../styles/style.css"
 import "../styles/utils.css"
 import loadHomePage from "./home-page"
-loadHomePage()
+const homeButton = document.getElementById('button-home')
+const menuButton = document.getElementById('button-menu')
+const aboutButton = document.getElementById('button-about')
+function removeSelectedAll(){
+    homeButton.classList.remove('selected')
+    menuButton.classList.remove('selected')
+    aboutButton.classList.remove('selected')
+}
+homeButton.addEventListener('click', () =>{
+    removeSelectedAll()
+    homeButton.classList.add('selected')
+    loadHomePage()
+})
