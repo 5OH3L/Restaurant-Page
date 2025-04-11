73b7870a96eb8fae6fed0c8f8f3bdd8349b70759
Sohel <152502004+5OH3L@users.noreply.github.com>
2025-04-11 19:06:04 +0530

    Add image credit pop up

---
+++ src/scripts/image-credits.js
@@ -0,0 +1,51 @@
+const imageCreditsOverlay = document.getElementById('image-credits-overlay')
+const imageCreditsMain = document.getElementById('image-credits-main')
+const imageCreditsMainCredits = document.getElementById('image-credits-main-credits')
+
+
+import ImageDetails from "./image-details"
+
+function showPopUp(){
+    imageCreditsOverlay.classList.remove('active')
+    imageCreditsMain.classList.remove('active')
+    imageCreditsOverlay.classList.add('active')
+    imageCreditsMain.classList.add('active')
+    imageCreditsMain.focus()
+    document.body.style.overflow = "hidden"
+}
+function closePopUp(){
+    imageCreditsOverlay.classList.remove('active')
+    imageCreditsMain.classList.remove('active')
+    document.body.style.overflow = "auto"
+}
+
+function addImageCredits(){
+    imageCreditsMainCredits.innerHTML = ""
+
+    ImageDetails.forEach( image => {
+        const imageCreditsMainCreditsImage = document.createElement('div')
+        imageCreditsMainCreditsImage.style.backgroundImage = `url(${image.localURL})`
+        imageCreditsMainCreditsImage.classList.add('image-credits-main-image')
+
+        const imageCreditsMainCreditsImageLink = document.createElement('a')
+        imageCreditsMainCreditsImageLink.href = image.creditURL
+        imageCreditsMainCreditsImageLink.target = "_blank"
+        imageCreditsMainCreditsImageLink.rel = "noopener noreferrer"
+        imageCreditsMainCreditsImageLink.classList.add('font-montserrat', 'font-weight-bold')
+        imageCreditsMainCreditsImage.appendChild(imageCreditsMainCreditsImageLink)
+
+        const imageCreditsMainCreditsImageCredit = document.createElement('h1')
+        imageCreditsMainCreditsImageCredit.textContent = image.credit
+        imageCreditsMainCreditsImageLink.appendChild(imageCreditsMainCreditsImageCredit)
+
+        imageCreditsMainCredits.appendChild(imageCreditsMainCreditsImage)
+    })
+}
+
+const imageCredits = {
+    refresh: addImageCredits,
+    showPopUp,
+    closePopUp
+}
+
+export default imageCredits
